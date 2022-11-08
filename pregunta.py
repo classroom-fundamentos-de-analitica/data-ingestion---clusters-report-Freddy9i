03de6fe8339cd7611a2510a918e4f7984009b5fc
Jhon Freddy Guerra Martínez <70671847+Freddy9i@users.noreply.github.com>
2022-11-08 04:34:13 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,68 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    datos = open("clusters_report.txt", mode='r')
+    columnas = ["", "", "", ""]
 
+    for i in range(2):
+        columna_index = i
+        num_espacios = 0
+        for char in datos.readline().strip():
+            if char!=' ':
+                if num_espacios == 1:
+                    columnas[columna_index] += '_'
+                elif num_espacios > 1:
+                    columna_index += 1
+                num_espacios = 0
+                columnas[columna_index] += char
+            else:
+                num_espacios +=1
+        
+        if i==0:
+            columnas[1] += '_'
+            columnas[2] += '_'
+    
+    columnas = [i.lower() for i in columnas]
+    datos_organizados= open("datos_organizados.txt","w+")
+    datos_organizados.write(";".join(columnas)+"\n")
+
+
+    for i in range(2): #saltarse lineas 3 y 4
+        next(datos)
+
+    ultimo_char = ''
+    for linea in datos.readlines():
+        if len(linea.lstrip(" "))>1:
+            linea = " "+linea.strip()
+        fila_organizada = ""
+        num_espacios = 0
+        
+        for char in linea:
+            if char!=' ':
+                if num_espacios==1:
+                    fila_organizada += ' '
+                elif num_espacios>1:
+                    if ultimo_char!="%" and not ultimo_char.isdecimal():
+                        fila_organizada += " "
+                    else:
+                        fila_organizada += ";"
+                num_espacios = 0
+                ultimo_char = char
+                fila_organizada += char
+
+            elif len(fila_organizada)>0:
+                num_espacios +=1
+
+        fila_organizada = fila_organizada.replace(",;", ", ")
+        fila_organizada = fila_organizada.replace(" %", "")
+        datos_organizados.write(fila_organizada)
+    
+    datos_organizados.close()
+    df = pd.read_csv("datos_organizados.txt", sep=";")
     return df
+
+print(ingest_data())
